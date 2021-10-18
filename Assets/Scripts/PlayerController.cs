8eec686b1ff59ae72917ca972f578cb28f6be3b9
felipeborgessilva <70147361+felipeborgessilva@users.noreply.github.com>
2021-10-18 00:53:24 -0300

    Adicionado movimentação de flip do personagem

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,64 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private Rigidbody2D rig;
+    private Animator anim;
+
+    public float speed;
+    public float jumpForce;
+    private float movement;
+
+    private float horizontal;
+    private float vertical;
+
+
+    public bool isLookingLeft;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rig = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        horizontal = Input.GetAxis("Horizontal");
+        float speedY = rig.velocity.y;
+
+        if(horizontal > 0 && isLookingLeft) 
+        {
+            Flip();
+        } else if(horizontal < 0 && !isLookingLeft) 
+        {
+            Flip();
+        }
+
+
+        if (horizontal != 0)
+        {
+            anim.SetBool("walking", true);
+        } else {
+            anim.SetBool("walking", false);
+        }
+        if(Input.GetButtonDown("Jump"))
+        {
+            rig.AddForce(new Vector2(0f, jumpForce));
+        }
+        rig.velocity = new Vector2(horizontal * speed, speedY);
+
     }
+
+    void Flip() {
+        anim.SetTrigger("flipping");
+        isLookingLeft = !isLookingLeft;
+        float x = transform.localScale.x * -1; 
+        transform.localScale = new Vector3(x, transform.localScale.y, transform.localScale.z);
+    }
+
+    void FixedUpdate() {
+
+    }
+
+
 }
